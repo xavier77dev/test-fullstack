4e35c55fe4fca9d6069d93f1a211cb8003783e6c
xavier77dev <xavier.basurto.77@gmail.com>
2024-09-06 21:41:41 -0500

    config: cors changes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -12,9 +12,6 @@ app.use(express.json());
 
 app.use(cors({
   origin: "*",
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true,
-  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 connectDB();
